68b032153933169fdfade141805f9fbbe3bcba10
Marcelo Corpucci <mcorpucci@gmail.com>
2017-07-30 17:10:45 -0300

    sesion 2. if, else, swicth.

--- src/main/java/com/effectivetesting/examples/CarDriver.java
+++ src/main/java/com/effectivetesting/examples/CarDriver.java
@@ -4,5 +4,23 @@ public class CarDriver {
 	public static void main(String[] args) {
 		Car myCar = new Car();
 		System.out.println(myCar.type);
+		
+		if(myCar.isDiesel == true) {
+			System.out.println("Diesel engine. Use fuel Euro 4.");
+		} else {
+			System.out.println("Gasoline engine.");
+		}
+		
+		switch (myCar.type) {
+	  	case "Sedan":
+	  		System.out.println("Doors: 4");
+	  		break;
+	  	case "Hatchback":
+	  		System.out.println("Doors: 3");
+	  		break;
+	  	case "other":
+	  		System.out.println("not determined");
+	  }
+
 	}
 }
