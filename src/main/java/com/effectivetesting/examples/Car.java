1c7f66a97527fc34b4618e36decdf47d9ee4e901
Marcelo Corpucci <mcorpucci@gmail.com>
2017-07-30 16:59:23 -0300

    Sesion 2. Clases, objetos y tipos de datos.

---
+++ src/main/java/com/effectivetesting/examples/Car.java
@@ -0,0 +1,7 @@
+package com.effectivetesting.examples;
+
+public class Car {
+	public int wheels = 4;
+	public String type = "Sedan";
+	public boolean isDiesel = false;
+}
---
+++ src/main/java/com/effectivetesting/examples/CarDriver.java
@@ -0,0 +1,8 @@
+package com.effectivetesting.examples;
+
+public class CarDriver {
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		System.out.println(myCar.type);
+	}
+}
