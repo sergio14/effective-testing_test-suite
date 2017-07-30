92eb6f41cb69985f7327efa302518dee5ee29340
Marcelo Corpucci <mcorpucci@gmail.com>
2017-07-30 19:40:44 -0300

    ejemplo sobre metodos en oop.

--- src/main/java/com/effectivetesting/examples/Car.java
+++ src/main/java/com/effectivetesting/examples/Car.java
@@ -1,9 +1,18 @@
 package com.effectivetesting.examples;
 
 public class Car {
-	public int wheels = 4;
-	public String type = "Sedan";
-	public boolean isDiesel = false;
-	public TireSensor tireSensor = new TireSensor();
-	public float fuelLevel;
+	private int rpm;	
+	
+    public Car() {
+    	rpm = 0;
+	}
+	
+	public int getCurrentRmp() {
+		return rpm;
+	}
+	
+	public void startEngine() {
+		rpm = 1000;
+		System.out.println("Engine started.");
+	}
 }
--- src/main/java/com/effectivetesting/examples/CarDriver.java
+++ src/main/java/com/effectivetesting/examples/CarDriver.java
@@ -3,21 +3,7 @@ package com.effectivetesting.examples;
 public class CarDriver {
 	public static void main(String[] args) {
 		Car myCar = new Car();
-		System.out.println("New car created.");
-		myCar.tireSensor.pressure = 29.3f;
-		
-		for(int i = 0; i < myCar.wheels; i++) {
-			if(myCar.tireSensor.pressure > 27.0f) {
-				System.out.println("Tire pressure level: normal");
-			}
-		}
-		
-		myCar.fuelLevel = 15.0f;
-		
-		while(myCar.fuelLevel > 0) {
-			System.out.println("Engine working. Fuel level: " + Float.toString(myCar.fuelLevel));
-			myCar.fuelLevel -= 0.5f;
-		}
-
+		myCar.startEngine();
+		System.out.println("Current rpms: " + myCar.getCurrentRmp());
 	}
 }
